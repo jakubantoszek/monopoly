5bcb4b5d799b11bec1629bdf0e4d923c0ab9bc0f
jakubantoszek <102921899+jakubantoszek@users.noreply.github.com>
2026-07-25 17:58:56 +0000

    Dodanie bazy danych - part 1

--- Src/Pole.py
+++ Src/Pole.py
@@ -2,13 +2,13 @@ from Grupa import Grupa
 from Gracz import Gracz
 
 class Pole:
-    cena: int
     nazwa: str
     numer: int
+    cena: int
+    oplaty: list
     domki: int
     grupa: Grupa
     posiadacz: Gracz
-    oplaty: list
 
     def __init__(self):
         pass
